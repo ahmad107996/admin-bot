a644c921149c9c6402dac62748fa8e07ccdf7820
ahmad107996 <36535354+ahmad107996@users.noreply.github.com>
2018-12-01 19:51:52 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,8 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const fs = require("fs");
+const moment = require("moment");
+
 var prefix = "#";
 client.on('ready', () => {
    console.log(`----------------`);
