46e5939ea27280aabe65d106c41005ae2bff591f
ahmad107996 <36535354+ahmad107996@users.noreply.github.com>
2019-01-04 20:55:34 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,8 +11,43 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`#help ||By - Saudi Shop TM||`,"https://www.twitch.tv/ahmad107996")
+client.user.setGame(`#help`,"https://www.twitch.tv/ahmad107996")
 client.user.setStatus("dnd")
+});
+
+const devs = ["348953140315291649"];
+const adminprefix = ["#"];
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!devs.includes(message.author.id)) return;
+      
+  if (message.content.startsWith('#ply')) {
+    client.user.setGame(argresult);
+      message.channel.send(`**✅   ${argresult}**`)
+  } else 
+     if (message.content === ("#leave")) {
+    message.guild.leave();        
+  } else  
+  if (message.content.startsWith('#wt')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.send(`**✅   ${argresult}**`)
+  } else 
+  if (message.content.startsWith('#mils')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send(`**✅   ${argresult}**`)
+  } else 
+  if (message.content.startsWith('#st')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/idk");
+      message.channel.send(`**✅**`)
+  }
+  if (message.content.startsWith('#setname')) {
+  client.user.setUsername(argresult).then
+      message.channel.send(`Changing The Name To ..**${argresult}** `)
+} else
+if (message.content.startsWith('#setavatar')) {
+  client.user.setAvatar(argresult);
+    message.channel.send(`Changing The Avatar To :**${argresult}** `);
+}
 });
 
 client.on("message", message => {
