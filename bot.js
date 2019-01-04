afcedcaa032180deeb86d900e3b8d9948914ee04
ahmad107996 <36535354+ahmad107996@users.noreply.github.com>
2019-01-04 21:04:16 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -424,19 +424,29 @@ client.on("message", async message => {
       }
     });
 
-client.on('message', function(message) {
-    if (message.content == "#clear") {
-        if (message.member.hasPermission("MANAGE_MESSAGES")) {
-            message.channel.fetchMessages()
-               .then(function(list){
-                    message.channel.bulkDelete(list);
-                }, function(err){message.channel.send("ERROR: ERROR CLEARING CHANNEL.")})
-		               message.reply("تم الحذف!")
-	}
-    }
+client.on('message', msg => {
+  if (msg.author.bot) return;
+  if (!msg.content.startsWith(prefix)) return;
+  let command = msg.content.split(" ")[0];
+  command = command.slice(prefix.length);
+  let args = msg.content.split(" ").slice(1);
 
-});
-	
+    if(command === "clear") {
+        const emoji = client.emojis.find("name", "wastebasket")
+    let textxt = args.slice(0).join("");
+    if(msg.member.hasPermission("MANAGE_MESSAGES")) {
+    if (textxt == "") {
+        msg.delete().then
+    msg.channel.send("***```ضع عدد الرسائل التي تريد مسحها :ok_hand:```***").then(m => m.delete(3000));
+} else {
+    msg.delete().then
+    msg.delete().then
+    msg.channel.bulkDelete(textxt);
+        msg.channel.send("```php\nعدد الرسائل التي تم مسحها: " + textxt + "\n```").then(m => m.delete(3000));
+        }    
+    }
+}
+});	
   client.on('message', message => {
     if (message.content.startsWith("#link")) {
 
