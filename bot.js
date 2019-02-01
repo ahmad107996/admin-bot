e6bff05dbda82c67c253e4b782a72dc2548afcb8
ahmad107996 <36535354+ahmad107996@users.noreply.github.com>
2019-02-01 14:39:17 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1067,5 +1067,67 @@ client.on("guildMemberAdd", member => {
 }).catch(console.error)
 });
 
+if (!Discord) {
+    var Discord = require(`discord.js`);
+};
+if (!client) {
+    var client = new Discord.Client();
+}
+
+if (!fs) {
+    var fs = require(`fs`);
+}
+var data = JSON.parse(fs.readFileSync(`./data.json`, `UTF8`));
+if (!prefix) {
+    var prefix = data.prefix;
+} else {
+    prefix = data.prefix;
+};
+client.on(`message`, msg => {
+    if (msg.author.bot) return;
+    if (!msg.content.startsWith(prefix)) return;
+    var args = msg.content.slice(prefix.length).split(` `);
+    var command = `${args[0]}`;
+    switch (command) {
+        case `vip`:
+            if (msg.author.id !== data.ownerID) return;
+            var cmd = args[1];
+            if (!cmd) return msg.reply(`what command you are looking for ?`);
+            switch(cmd) {
+                case `move`:
+                    var guild = args[2];
+                    if (!guild) return msg.reply(`I can't find this server \\:(`);
+                    if (isNaN(parseInt(guild))) return msg.reply(`I can't find this server \\:(`);
+                    if (guild.length !== msg.guild.id.length) return msg.reply(`I can't find this server \\:(`);
+                    msg.channel.send(`invite me to your new server: https://discordapp.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&guild_id=${guild}&permissions=2146958847`);
+                    data.guildID = guild;
+                break;
+                case `prefix`:
+                    var prfx = args[3];
+                    if (!prefix) return msg.reply(`prefix ?`);
+                    data.prefix = prfx.trim();
+                    msg.channel.send(`done , my prefix now is : ${prfx}`);
+                break;
+            }
+        break;
+    };
+    fs.writeFileSync(`./data.json`, JSON.stringify(data, (null, 4)));
+});
+client.on(`guildCreate`, guild => {
+    if (guild.id !== data.guildID) guild.leave();
+    setTimeout(function Sweetie(){
+        client.guilds.forEach(g => {
+            if (g.id !== data.guildID) guild.leave();
+        });
+    }, 5000);
+});
+
+client.on(`ready`, () => {
+    client.guilds.forEach(guild => {
+        if (guild.id !== data.guildID) guild.leave();
+    });
+});
+
+client.login(`${data.token}`);
 
 client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
